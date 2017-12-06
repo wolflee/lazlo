ce84131557acef3518302d0c91bed76ca2e83cca
WolfLee <me@wolfl.ee>
2017-12-06 23:47:35 +0800

    Change env variable names

--- lib/config.go
+++ lib/config.go
@@ -9,13 +9,13 @@ import (
 
 // Config struct
 type Config struct {
-	Name     string `env:"key=LAZLO_NAME default=lazlo"`
-	Token    string `env:"key=LAZLO_TOKEN"`
-	URL      string `env:"key=LAZLO_URL default=http://localhost"`
-	LogLevel string `env:"key=LAZLO_LOG_LEVEL default=info"`
-	RedisURL string `env:"key=LAZLO_REDIS_URL"`
-	RedisPW  string `env:"key=LAZLO_REDIS_PW"`
-	Port     string `env:"key=PORT"`
+	Name     string `env:"key=CHATBOT_NAME default=lazlo"`
+	Token    string `env:"key=CHATBOT_TOKEN"`
+	URL      string `env:"key=CHATBOT_URL default=http://localhost"`
+	LogLevel string `env:"key=CHATBOT_LOG_LEVEL default=info"`
+	RedisURL string `env:"key=CHATBOT_REDIS_URL"`
+	RedisPW  string `env:"key=CHATBOT_REDIS_PW"`
+	Port     string `env:"key=CHATBOT_PORT"`
 }
 
 func newConfig() *Config {
@@ -28,6 +28,6 @@ func newLogger() *logging.Logger {
 	format := "%25s [%s] %8s: %s\n time,name,levelname,message"
 	timeFormat := time.RFC3339
 	level := logging.GetLevelValue(`INFO`)
-	logger, _ := logging.WriterLogger("lazlo", level, format, timeFormat, os.Stdout, true)
+	logger, _ := logging.WriterLogger("chatbot", level, format, timeFormat, os.Stdout, true)
 	return logger
 }
